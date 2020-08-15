0968654ac8ff50f63c7ec3acc67f17f6d09ec253
eunmo <eunmo@cs.stanford.edu>
2020-08-15 22:53:44 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### Link config file: `cd /etc/nginx && sudo ln -s <root_dir>/nginx.conf`
+
+### Restart nginx: `sudo nginx -s reload`
+
+### Restart servers: `./forever.sh`
---
+++ forever.sh
@@ -0,0 +1,9 @@
+dev_dir="/home/ubuntu"
+
+forever stopall
+# authbind --deep forever start --sourceDir $dev_dir/www main.js
+forever start --sourceDir $dev_dir/tango server/tango-server.js
+forever start --sourceDir $dev_dir/hexagon server/hexagon.js
+forever start --sourceDir $dev_dir/front_page server/front_page.js
+forever start --sourceDir $dev_dir/bible server/bible.js
+forever list
