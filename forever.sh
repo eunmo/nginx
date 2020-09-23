47510a4fb1e962a50a1ed8ab264335568976fb96
eunmo <eunmo@cs.stanford.edu>
2020-09-24 01:12:43 +0900

    add chart match

--- forever.sh
+++ forever.sh
@@ -2,6 +2,7 @@ dev_dir="/home/ubuntu"
 
 forever stopall
 # authbind --deep forever start --sourceDir $dev_dir/www main.js
+forever start --sourceDir $dev_dir/chart-match server/chart-match.js
 forever start --sourceDir $dev_dir/tango server/tango-server.js
 forever start --sourceDir $dev_dir/hexagon server/hexagon.js
 forever start --sourceDir $dev_dir/front_page server/front_page.js
